b2cdbb5be38069b3c8baac52857df2a14d33b779
Mercia Jeno <mercia.jeno@gmail.com>
2026-05-20 19:27:01 +0530

    learnt how to draw shapes

--- core/src/main/java/com/mygame/Main.java
+++ core/src/main/java/com/mygame/Main.java
@@ -5,6 +5,7 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
@@ -12,12 +13,13 @@ public class Main extends ApplicationAdapter {
     private SpriteBatch batch;
     private Texture image;
     private BitmapFont font;
-
+    private ShapeRenderer shape;
     @Override
     public void create() {
         batch = new SpriteBatch();
         image = new Texture("libgdx.png");
         font = new BitmapFont();
+        shape = new ShapeRenderer();
     }
 
     @Override
@@ -25,7 +27,10 @@ public class Main extends ApplicationAdapter {
         ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
         batch.begin();
         //batch.draw(image, 140, 210);
-        font.draw(batch,"Happy Coding!", Gdx.graphics.getWidth()/2, Gdx.graphics.getHeight()/2);
+        //font.draw(batch,"Happy Coding!", Gdx.graphics.getWidth()/2, Gdx.graphics.getHeight()/2);
+        shape.begin(ShapeRenderer.ShapeType.Line);
+        shape.circle(200, 100, 78);
+        shape.end();
         batch.end();
     }
 
