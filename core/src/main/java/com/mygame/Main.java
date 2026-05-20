fb417c97f60ce07135144e4272e6aad300d08e87
Mercia Jeno <mercia.jeno@gmail.com>
2026-05-20 19:23:03 +0530

    sample hello world

--- core/src/main/java/com/mygame/Main.java
+++ core/src/main/java/com/mygame/Main.java
@@ -1,7 +1,9 @@
 package com.mygame;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
@@ -9,18 +11,21 @@ import com.badlogic.gdx.utils.ScreenUtils;
 public class Main extends ApplicationAdapter {
     private SpriteBatch batch;
     private Texture image;
+    private BitmapFont font;
 
     @Override
     public void create() {
         batch = new SpriteBatch();
         image = new Texture("libgdx.png");
+        font = new BitmapFont();
     }
 
     @Override
     public void render() {
         ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
         batch.begin();
-        batch.draw(image, 140, 210);
+        //batch.draw(image, 140, 210);
+        font.draw(batch,"Happy Coding!", Gdx.graphics.getWidth()/2, Gdx.graphics.getHeight()/2);
         batch.end();
     }
 
